cd5006d8fc7e74349832f280cf3afa04c63e92a3
Sepang2 <sepang296@gmail.com>
2024-04-04 16:09:45 +0900

    Modify output msg

--- 02. Recursion/HanoiTower.c
+++ 02. Recursion/HanoiTower.c
@@ -4,11 +4,11 @@
 void HanoiTowerMove(int num, char from, char by, char to)
 {
     if (num == 1)
-        printf("move disk1 from %c to %c \n", from, to);
+        printf("move disk #1 from %c to %c \n", from, to);
     else
     {
         HanoiTowerMove(num - 1, from, to, by);
-        printf("move disk%d from %c to %c \n", num, from, to);
+        printf("move disk #%d from %c to %c \n", num, from, to);
         HanoiTowerMove(num - 1, by, from, to);
     }
 }
